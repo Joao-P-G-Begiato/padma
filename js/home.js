faffa377c69380bbaea5c37539138d53eb3aaa48
João Paulo Gotardi Begiato <jpbegiato@hotmail.com>
2023-12-12 12:50:58 -0300

    (feat) Criação da lógica e do link para página de gerenciamento de usuários

--- js/home.js
+++ js/home.js
@@ -2,12 +2,14 @@ const token = sessionStorage.getItem('token')
 const userName = sessionStorage.getItem('name')
 const isAdmin = sessionStorage.getItem('admin')
 
-
 document.addEventListener('DOMContentLoaded', function(){
+    if(isAdmin == "false" || !isAdmin){
+        console.log('oi')
+        document.getElementById('userManagment').remove()
+    }
     if(!token){
         window.open('../index.html', "_self")
     }
     const nameSpace = document.getElementById('name')
-    console.log(nameSpace)
     nameSpace.innerText = userName + " ! "
 })
