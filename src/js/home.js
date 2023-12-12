dac3bd881c93bcb3fd7f23a4558855f88d462812
João Paulo Gotardi Begiato <jpbegiato@hotmail.com>
2023-12-12 08:19:17 -0300

    implementação do dotenv e do jsonwebtoken

--- src/js/home.js
+++ src/js/home.js
@@ -1,10 +1,15 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const token = sessionStorage.getItem('token')
 const userName = sessionStorage.getItem('name')
-
+const validToken = jwt.verify(token , process.env.SECRET)
 const expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm_Do28iLCJyb2xlIjpmYWxzZSwiaWF0IjoxNzAyMzA1OTQ4LCJleHAiOjE3MDIzNDE5NDh9._yxYwDklcyFZr7OWQIhTdZQ48SQBA8RBwjtEPABJbYM"
 
 console.log(validToken)
-console.log(teste)
+console.log(jwt.verify(expiredToken))
 
 document.addEventListener('DOMContentLoaded', function(){
     if(!teste){
