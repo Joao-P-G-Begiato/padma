3aa758ac6e5f5279f0f3af0717d53ea7fdd706d6
João Paulo Gotardi Begiato <jpbegiato@hotmail.com>
2023-12-12 08:51:00 -0300

    (Fix) import do jsonwebtoken

--- js/home.js
+++ js/home.js
@@ -1,11 +1,11 @@
-import * as jwt from 'jsonwebtoken'
+import {verify} from 'jsonwebtoken'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const token = sessionStorage.getItem('token')
 const userName = sessionStorage.getItem('name')
-const validToken = jwt.verify(token , process.env.SECRET)
+const validToken = verify(token , process.env.SECRET)
 const expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm_Do28iLCJyb2xlIjpmYWxzZSwiaWF0IjoxNzAyMzA1OTQ4LCJleHAiOjE3MDIzNDE5NDh9._yxYwDklcyFZr7OWQIhTdZQ48SQBA8RBwjtEPABJbYM"
 
 console.log(validToken)
