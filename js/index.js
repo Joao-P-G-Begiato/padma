db3865406b992f0be4405b762d77ba94db786284
João Paulo Gotardi Begiato <jpbegiato@hotmail.com>
2023-12-12 08:46:33 -0300

    (Fix) redirecionamento da index

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ loginForm.addEventListener('submit', async function(e){
         const getToken = await getAuth(url)
         console.log(getToken)
         sessionStorage.setItem('token', getToken.token)
-        window.open("../../src/pages/home.html", "_self")
+        window.open("../../pages/home.html", "_self")
     }catch(e){
         const main = document.getElementsByTagName("main")
         const section = document.getElementsByTagName("section")[0]
